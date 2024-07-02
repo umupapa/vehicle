cc19e6eb38422890cccbe20deeff58f5cd52cdc9
umupapa <fatirutagengwa1@gmail.com>
2024-07-02 16:35:48 +0200

    commit

--- index.js
+++ index.js
@@ -22,11 +22,21 @@ const fetchDataFromAPI = async () => {
       if (item?.device_data?.traccar) {
         const { id, name, lastValidLatitude, lastValidLongitude } =
           item.device_data.traccar;
+        // const latitude = lastValidLatitude;
+        pool.query("DELETE FROM vehicle_test2 where objectid = $1", [id]);
         pool.query(
-          `INSERT INTO vehicles_data (
-              id, name, latitude, longitude, shape)
-              VALUES ($1, $2, $3, $4, $5)`,
-          [id, name, lastValidLatitude, lastValidLongitude, "circle"]
+          `INSERT INTO vehicle_test2 (
+              objectid, id, name, latitude, longitude, shape)
+              VALUES ($1, $2, $3, $4, $5, sde.st_geometry('POINT(' || $6 || ' ' || $7 || ')', 4326))`,
+          [
+            id,
+            id,
+            name,
+            lastValidLatitude,
+            lastValidLongitude,
+            lastValidLongitude,
+            lastValidLatitude,
+          ]
         );
         console.log(
           `Item ID: ${id}, lastValidLatitude: ${lastValidLatitude}, lastValidLongitude: ${lastValidLongitude}`
